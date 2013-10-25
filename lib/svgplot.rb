f4bd1e78667c2eaf2a0b1a69570d34e964b884ef
Les Aker <me@lesaker.org>
2013-10-24 22:45:44 -0400

    skeleton library

---
+++ lib/svgplot.rb
@@ -0,0 +1,21 @@
+##
+# SVG generation module
+
+module SVGPlot
+    Version = '0.0.1'
+    class << self
+        ##
+        # Add .new() helper for creating a new Plot object
+
+        def new(*args)
+            self::SVG.new(*args)
+        end
+    end
+
+    ##
+    # Plot object (represents an SVG image)
+
+    class Plot
+    end
+end
+
