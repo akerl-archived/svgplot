d85f33d95b527cf431ee2ed5a1988a33896b2052
Les Aker <me@lesaker.org>
2015-01-09 23:28:21 -0500

    correct requires

--- lib/svgplot.rb
+++ lib/svgplot.rb
@@ -14,5 +14,8 @@ end
 require 'svgplot/spec'
 require 'svgplot/application'
 require 'svgplot/meta'
+require 'svgplot/parsers'
 require 'svgplot/tag'
+require 'svgplot/gradient'
+require 'svgplot/transform'
 require 'svgplot/plot'
