6fa54bb5d5955fce173e995b7b099d0f6adbcfff
Les Aker <me@lesaker.org>
2015-04-01 21:12:34 -0400

    mock out specs

--- spec/svgplot_spec.rb
+++ spec/svgplot_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe SVGPlot do
-  it 'works' do
-    expect(true).to be true
+  describe '#new' do
+    it 'creates a Plot object' do
+      expect(SVGPlot.new).to be_an_instance_of SVGPlot::Plot
+    end
   end
 end
